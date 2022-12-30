fe4b8a961d427b20d6c471b9851e3eb47a7767f6
Harsit-Agarwalla <harsitagarwalla187@gmail.com>
2022-12-30 17:45:33 +0530

    Add Keeper App Part 1

---
+++ Keeper App/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+let date = new Date();
+let year = date.getFullYear();
+
+function Footer() {
+     return (
+          <footer>
+               <p>Copyright©{year}</p>
+          </footer>
+     );
+}
+
+export default Footer;
